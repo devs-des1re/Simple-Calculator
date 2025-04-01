f0be06bd89b760f3f34c426ff8c346c059c4515b
devs_des1re <arjunbrij8811@gmail.com>
2025-04-01 11:58:01 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,77 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.core.window import Window
+from kivy.lang.builder import Builder
+
+Builder.load_file("main.kv")
+Window.size = (350, 500)
+
+class MainWidget(Widget):
+    def clear(self):
+        self.ids.answer_box.text = "0"
+
+    def number_pressed(self, number):
+        prev_number = self.ids.answer_box.text
+
+        if prev_number == "0":
+            self.ids.answer_box.text = f"{number}"
+        else:
+            self.ids.answer_box.text = f"{prev_number}{number}"
+
+    def sign_pressed(self, sign):
+        prev_number = self.ids.answer_box.text
+        if prev_number[-1] == "÷" or prev_number[-1] == "×" or prev_number[-1] == "+" or prev_number[-1] == "-":
+            prev_number = prev_number[:-1]
+            self.ids.answer_box.text = f"{prev_number}{sign}"
+        else:
+            self.ids.answer_box.text = f"{prev_number}{sign}"
+
+    def pn_pressed(self):
+        prev_number = self.ids.answer_box.text
+        
+        if prev_number != "0":
+            if "-" in prev_number:
+                self.ids.answer_box.text = f"{prev_number.replace("-", "")}"
+            else:
+                self.ids.answer_box.text = f"-{prev_number}"
+    
+    def dm_pressed(self):
+        prev_number = self.ids.answer_box.text
+        if not prev_number[-1] == ".":
+            self.ids.answer_box.text = f"{prev_number}."
+
+    def delete_pressed(self):
+        prev_number = self.ids.answer_box.text
+        if prev_number != "0":
+            prev_number = prev_number[:-1]
+            if prev_number == "":
+                self.ids.answer_box.text = "0"
+            else:
+                self.ids.answer_box.text = prev_number
+
+    def equals_pressed(self):
+        text = self.ids.answer_box.text
+        text = text.replace("%", "/100")
+        text = text.replace("×", "*")
+        text = text.replace("÷", "/")
+
+        try:
+            answer = eval(text)
+            if isinstance(answer, float) and answer.is_integer():
+                self.ids.answer_box.text = str(int(answer))
+            else:
+                self.ids.answer_box.text = str(answer)
+        except SyntaxError:
+            self.ids.answer_box.text = "Syntax Error"
+        except ZeroDivisionError:
+            self.ids.answer_box.text = "Math Error"
+        except:
+            self.ids.answer_box.text = "Math Error"
+
+
+class MainApp(App):
+    def build(self):
+        return MainWidget()
+    
+if __name__ == "__main__":
+    MainApp().run()
